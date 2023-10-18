0af2aa62a967efa420f658e86db01e08c9355c50
chumyshhh[llI] <vandarkslave@outlook.com>
2023-10-18 10:06:58 +0300

    Initial

---
+++ app/src/main/java/com/heicos/App.kt
@@ -0,0 +1,17 @@
+package com.heicos
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.util.DebugLogger
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader(this)
+            .newBuilder()
+            .logger(DebugLogger())
+            .build()
+    }
+}
---
+++ app/src/main/java/com/heicos/presentation/main/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.heicos.presentation.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.heicos.ui.theme.HeicosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HeicosTheme {
+                val systemUIController = rememberSystemUiController()
+                val navigationBarColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                val statusBarColor = MaterialTheme.colorScheme.surface
+                val isDarkTheme = isSystemInDarkTheme()
+
+                SideEffect {
+                    systemUIController.setNavigationBarColor(
+                        color = navigationBarColor,
+                        navigationBarContrastEnforced = false,
+                    )
+                    systemUIController.setStatusBarColor(
+                        color = statusBarColor,
+                        darkIcons = !isDarkTheme
+                    )
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(text = "Test")
+                }
+            }
+        }
+    }
+}
