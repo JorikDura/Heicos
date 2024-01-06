d4e3891eea1775120edd66e55ae8b38ff1103d25
chumyshhh[llI] <vandarkslave@outlook.com>
2024-01-06 03:42:43 +0300

    updated deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -96,9 +96,9 @@ dependencies {
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     //navigation
-    implementation("androidx.navigation:navigation-compose:2.7.5")
-    implementation("io.github.raamcosta.compose-destinations:animations-core:1.9.54")
-    ksp("io.github.raamcosta.compose-destinations:ksp:1.9.54")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("io.github.raamcosta.compose-destinations:animations-core:1.9.55")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.9.55")
     implementation("com.google.accompanist:accompanist-navigation-material:0.32.0")
     //ui
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
@@ -108,7 +108,7 @@ dependencies {
     //material
     implementation("androidx.compose.material3:material3:1.1.2")
     //room
-    implementation("androidx.room:room-runtime:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
