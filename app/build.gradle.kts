56645a94647b11c52cc36d67a1758dce4f479af9
chumyshhh[llI] <vandarkslave@outlook.com>
2024-02-21 18:54:25 +0300

    version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "1.5"
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
