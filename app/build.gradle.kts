3b0874b6bbef2904fbaecfce1058a2f8fa68999a
chumyshhh[llI] <vandarkslave@outlook.com>
2024-02-25 22:48:33 +0300

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     //viewmodel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     //hilt
-    implementation("com.google.dagger:hilt-android:2.48")
+    implementation("com.google.dagger:hilt-android:2.49")
     kapt("com.google.dagger:hilt-android-compiler:2.46.1")
     kapt("androidx.hilt:hilt-compiler:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
