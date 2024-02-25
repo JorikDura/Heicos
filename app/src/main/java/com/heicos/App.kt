28534312ccacb2c3ec230e225ddc8dded52a5ff8
chumyshhh[llI] <vandarkslave@outlook.com>
2024-02-25 21:56:01 +0300

    moved gif setting to app file

--- app/src/main/java/com/heicos/App.kt
+++ app/src/main/java/com/heicos/App.kt
@@ -1,8 +1,11 @@
 package com.heicos
 
 import android.app.Application
+import android.os.Build
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,6 +14,13 @@ class App : Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader {
         return ImageLoader(this)
             .newBuilder()
+            .components {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }
             .logger(DebugLogger())
             .build()
     }
