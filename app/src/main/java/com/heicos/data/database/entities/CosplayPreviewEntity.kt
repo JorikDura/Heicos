999f03177a4d8ab62b466fcd5da268db10cbb684
JorikDura <vandarkslave@outlook.com>
2024-10-27 04:16:02 +0300

    remove import

--- app/src/main/java/com/heicos/data/database/entities/CosplayPreviewEntity.kt
+++ app/src/main/java/com/heicos/data/database/entities/CosplayPreviewEntity.kt
@@ -2,9 +2,10 @@ package com.heicos.data.database.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(indices = [Index(value = ["name"], unique = true)])
 data class CosplayPreviewEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
