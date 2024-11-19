eaa81571c1f9cc2d69612f8b5fed9f8a26999ac4
JorikDura <vandarkslave@outlook.com>
2024-11-20 00:24:30 +0300

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "1.9.1"
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
